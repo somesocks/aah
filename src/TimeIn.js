b163561980a500e80fa88076d5ea80d1a5b880dc
James L <somesocks@gmail.com>
2018-06-22 10:37:50 +0200

    adding TimeIn

---
+++ src/TimeIn.js
@@ -0,0 +1,32 @@
+
+const Delay = require('./Delay');
+const InSeries = require('./InSeries');
+const InParallel = require('./InParallel');
+
+const PassThrough = require('./PassThrough');
+
+/**
+* ```javascript
+	const task = TimeIn(async (i) => i + 1, 1000);
+
+	const result = await task(1); // result1 = 2, after 1000 ms
+* ```
+*
+* @name TimeIn
+* @param {function} task - an async task
+* @param {function} timeIn - the minimum time the task can take
+* @returns {function} an async task
+* @memberof aah
+*/
+const TimeIn = function (task, timeIn) {
+	task = task || PassThrough;
+	timeIn = timeIn || 0;
+	const timeInTask = Delay(timeIn);
+
+	return InSeries(
+		InParallel(task, timeInTask),
+		([ res1, res2 ]) => res1
+	)
+};
+
+module.exports = TimeIn;
---
+++ src/TimeIn.tests.js
@@ -0,0 +1,31 @@
+
+import * as Vet from 'vet';
+const { isFunction } = Vet;
+
+import test from 'ava';
+
+import { TimeIn } from '../dist/aah.dev';
+
+test(
+	'TimeIn is a function',
+	(test) => {
+		test.true(isFunction(TimeIn));
+	}
+);
+
+test(
+	'TimeIn works 1',
+	async (test) => {
+
+		const task = TimeIn(async (i) => i + 1, 100);
+
+		const start = Date.now();
+
+		const res = await task(0);
+
+		const end = Date.now();
+
+		test.true(res === 1);
+		test.true((end - start) >= 100);
+	}
+);
